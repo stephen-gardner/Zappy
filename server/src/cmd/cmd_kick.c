d4801e6b3f5eca97a881d49b724fe80914045f8e
Stephen Gardner <stephenbgardner@gmail.com>
2018-06-15 19:56:04 -0700

    Move array into function scope

--- server/src/cmd/cmd_kick.c
+++ server/src/cmd/cmd_kick.c
@@ -6,27 +6,26 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 22:29:11 by sgardner          #+#    #+#             */
-/*   Updated: 2018/06/12 23:32:03 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/06/14 22:21:08 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "zappy.h"
 
-const char	*g_kickdir[4] = {
-	"5713",
-	"3571",
-	"1357",
-	"7135"
-};
-
 static void	kick_player(t_serv *s, int id, int dir)
 {
-	t_ent		*ent;
+	static const char	*kickdir[4] = {
+		"5713",
+		"3571",
+		"1357",
+		"7135"
+	};
+	t_ent				*ent;
 
 	ent = ENT(s, id);
 	move_dir(s, ent, dir);
-	dprintf(SOCK(s, id), "moving %c\n", g_kickdir[dir][ent->facing]);
+	dprintf(SOCK(s, id), "moving %c\n", kickdir[dir][ent->facing]);
 }
 
 void		cmd_kick(t_serv *s, int id)
