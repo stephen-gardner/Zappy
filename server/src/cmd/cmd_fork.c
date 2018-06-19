78fba9df5b855e0ebdf27445e16c5e407ff243d8
Stephen Gardner <stephenbgardner@gmail.com>
2018-06-19 10:33:45 -0700

    Allow fork to go through even if exceeding item limit

--- server/src/cmd/cmd_fork.c
+++ server/src/cmd/cmd_fork.c
@@ -6,7 +6,7 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 20:17:58 by sgardner          #+#    #+#             */
-/*   Updated: 2018/06/19 00:02:13 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/06/19 10:32:33 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	cmd_fork(t_serv *s, int id, t_ent *ent, t_buff *buff)
 
 	(void)id;
 	loc = GET_LOC(s, ent->loc_x, ent->loc_y);
-	if (modify_resource(loc, EGG, 1) < 0)
-		return ;
+	modify_resource(loc, EGG, 1);
 	add_egg(s, ent->team, ent->loc_x, ent->loc_y);
 	OK(buff);
 }
